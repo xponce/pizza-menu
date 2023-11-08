bb1bc6208a1e1aeb917132800a3c7fd7a443afc8
Xavier Ponce <xaviergponce@gmail.com>
2023-11-07 16:40:17 -0800

    Final Project

--- src/index.js
+++ src/index.js
@@ -75,12 +75,20 @@ function Menu() {
         <main className="menu">
             <h2>Our Menu</h2>
 
+
+
             {numPizzas > 0 ? (
-                <ul className="pizzas">
-                    {pizzas.map((pizza) => (
-                        <Pizza pizzaObj={pizza} key={pizza.name} />
-                    ))}
-                </ul>
+                <>
+                    <p>
+                        Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.
+                    </p>
+
+                    <ul className="pizzas">
+                        {pizzas.map((pizza) => (
+                            <Pizza pizzaObj={pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </>
             ) : (
                 <p>We're still working on our menu. Please come back later :)</p>
             )}
@@ -104,15 +112,15 @@ function Menu() {
 function Pizza({ pizzaObj }) {
     console.log(pizzaObj);
 
-    if (pizzaObj.soldOut) return null;
+    // if (pizzaObj.soldOut) return null;
 
     return (
-        <li className="pizza">
+        <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
             <img src={pizzaObj.photoName} alt={pizzaObj.name} />
             <div>
                 <h3>{pizzaObj.name}</h3>
                 <p>{pizzaObj.ingredients}</p>
-                <span>{pizzaObj.price}</span>
+                <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
             </div>
         </li>
     );
